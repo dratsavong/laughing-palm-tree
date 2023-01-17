36b4ec1e38a272541db3c96233833e202b05c433
dratsavong <dratsavong@ymail.com>
2023-01-17 18:06:16 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require('path');
 const PORT = process.env.port || 3001;
 const app = express();
 
+let notes = require("./db/db.json");
+
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,8 +24,24 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+//POST route for creating a note
+app.post("/api/notes", (req, res) => {
+  const note = createNote (req.body, notes);
+  res.json(note)
+});
 
+//function to create a note 
+function createNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync (
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(notesArray, 2)
+  );
 
+  return note;
+};
+  
 app.listen(PORT, function() {
     console.log(`Server is listening on PORT: ${PORT}`);
   });
